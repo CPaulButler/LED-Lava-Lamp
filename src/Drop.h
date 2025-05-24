5d7bd3d49c2a3d28e6cd3d5a5f3218d73abb3255
Paul Butler <c.paul.butler@gmail.com>
2025-05-23 22:00:11 -0500

    add a mix of rising/falling lights

--- src/Drop.h
+++ src/Drop.h
@@ -10,22 +10,32 @@
     CRGB c;
   };
 
-  #define litLEDCount 3
+  #define litLEDCount 5
   static litLED litLEDs[litLEDCount];
   static int nextLED = 1;
 
   litLED updateLED(litLED l) {
     litLED retL;
-    retL.x = l.x + l.dx;
+
+    retL.dx = l.dx;
     retL.y = l.y;
     retL.c = l.c; //.fadeToBlackBy(20);
+
+    // Serial.printf("l.x = %4.1f, l.dx = %4.1f ", l.x, l.dx);
+    retL.x = l.x + l.dx;
     if (retL.x < 0.0) {
-      /*
-      Serial.printf("%4.1f < 0, ", retL.x);
-      Serial.printf("x = %4.1f, y = %4.1f, r = %3d, g = %3d, b = %3d\n", retL.x, retL.y, retL.c.r, retL.c.g, retL.c.b);
-      */
+      // Serial.printf("too  low ");
+      retL.x = 0.0;
       retL.c = CRGB::Black;
     }
+
+    if (retL.x > kMatrixWidth-1) {
+      // Serial.printf("too high ");
+      retL.x = kMatrixWidth - 1;
+      retL.c = CRGB::Black;
+    }
+
+    // Serial.printf("retL.x = %4.1f\n", retL.x);
     return retL;
   }
 
@@ -34,24 +44,24 @@
     float distanceFromBottom = l.x;
     if (distanceFromBottom <= fadeSlope) {
       uint8_t rval = (uint8_t) ((fadeSlope - distanceFromBottom) / fadeSlope * 255.0);
-      //Serial.printf("l.x = %4.1f, distanceFromBottom = %4.1f, rval = %d\n", l.x, distanceFromBottom, rval);
+      // Serial.printf("l.x = %4.1f, distanceFromBottom = %4.1f, rval = %d\n", l.x, distanceFromBottom, rval);
       return (uint8_t) ((fadeSlope - distanceFromBottom) / fadeSlope * 255.0);
     }
 
     float distanceFromTop = ((float) kMatrixWidth - 1) - l.x;
     if (distanceFromTop <= fadeSlope) {
       uint8_t rval = (uint8_t) ((fadeSlope - distanceFromTop) / fadeSlope * 255.0);
-      //Serial.printf("l.x = %4.1f, distanceFromTop = %4.1f, rval = %d\n", l.x, distanceFromTop, rval);
+      // Serial.printf("l.x = %4.1f, distanceFromTop = %4.1f, rval = %d\n", l.x, distanceFromTop, rval);
       return rval;
     }
 
-    //Serial.printf("l.x = %4.1f\n", l.x);
+    // Serial.printf("l.x = %4.1f\n", l.x);
     return 0;
   }
 
   int emptyLED() {
     for (int i = 0; i < litLEDCount; i++){
-      if (litLEDs[i].x <= 0) {
+      if ((litLEDs[i].x == 0.0) || (litLEDs[i].x == kMatrixWidth-1)) {
         //Serial.printf("empty at %d\n",i);
         return i;
       }
@@ -67,12 +77,19 @@
     CRGB c = CRGB(0, 0, 0);
 
     EVERY_N_MILLISECONDS(300) {
-      x = (float) (kMatrixWidth - 1);
-      dx = -((float) random(100)) / 150.0 - 0.50;
+      dx = ((float) random(100)) / 200.0 + 0.15;
+      if (random(100) > 85) dx = -dx;
+
+      if (dx < 0.0) {
+        x = (float) (kMatrixWidth - 1);
+      } else {
+        x = 0.0;
+      }
+
       y = random(kMatrixHeight - 1);
-      uint8_t r = random(128);
-      uint8_t g = random(128);
-      uint8_t b = random(128);
+      uint8_t r = random(255);
+      uint8_t g = random(255);
+      uint8_t b = random(255);
       c = CRGB(r, g, b);
 
       nextLED = emptyLED();
